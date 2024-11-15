bf2f6347d6e2d0e81ea9144cf4619180c7833672
puerchik <rumyantsevnn@yandex.ru>
2024-11-15 12:46:20 +0300

    modal window layout and styles

--- js/main.js
+++ js/main.js
@@ -46,3 +46,37 @@ displaySelected('form-group', 'form-group-wrapper')
 displaySelected('form-coordinating', 'form-coordinating-wrapper')
 displaySelected('form-who-opened', 'form-who-opened-wrapper')
 displaySelected('form-created-by', 'form-created-by-wrapper')
+
+displaySelected('form-responsible-modal', 'form-responsible-modal-wrapper')
+displaySelected('form-group-modal', 'form-group-modal-wrapper')
+displaySelected('form-coordinating-modal', 'form-coordinating-modal-wrapper')
+displaySelected('form-who-opened-modal', 'form-who-opened-modal-wrapper')
+displaySelected('form-created-by-modal', 'form-created-by-modal-wrapper')
+// modal window
+
+const dialog = document.getElementById('content-modal')
+const dialogOpener = document.getElementById('dialog-opener')
+const dialogCloseButton = document.getElementById('modal-close-button')
+
+function openModalAndLockScroll() {
+    dialog.showModal()
+    document.body.classList.add('scroll-lock')
+}
+
+function returnScroll() {
+    document.body.classList.remove('scroll-lock')
+}
+
+
+function closeOnBackDropClick({ currentTarget, target }) {
+    const dialogElement = currentTarget
+    const isClickedOnBackDrop = target === dialogElement
+    if (isClickedOnBackDrop) {
+        dialogElement.close()
+    }
+}
+
+dialogOpener.addEventListener('click', openModalAndLockScroll)
+dialogCloseButton.addEventListener('click', () => { dialog.close() })
+dialog.addEventListener('close', returnScroll)
+dialog.addEventListener('click', closeOnBackDropClick)
