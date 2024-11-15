a1a65c58973f982c65aa7beed511e998866507f9
puerchik <rumyantsevnn@yandex.ru>
2024-11-15 10:56:01 +0300

    form layout and styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// shadow while scrolling
+
 window.addEventListener('scroll', function () {
     const contentHeader = document.querySelector('.content__header')
 
@@ -8,16 +10,39 @@ window.addEventListener('scroll', function () {
     }
 })
 
-const responsibleSelect = document.getElementById('form-responsible')
-const responsibleSelectOptions = Array.from(responsibleSelect.selectedOptions)
-const responsibleSelectValues = responsibleSelectOptions.map(option => option.outerText)
 
-const formResponsiblWwrapper = document.getElementById('form-responsible-wrapper')
+// display of selected elements of the select
 
-responsibleSelectValues.forEach(item => {
-    const p = document.createElement('p')
-    p.textContent = item
-    p.classList.add('form__select-selected')
-    formResponsiblWwrapper.appendChild(p)
-})
+function displaySelected(selectId, wrapperId) {
+    const select = document.getElementById(selectId)
+    const selectedOptions = Array.from(select.selectedOptions)
+    const selectedOptionsText = selectedOptions.map(option => option.outerText)
+    const isMultiple = select.multiple
+    const wrapper = document.getElementById(wrapperId)
 
+    selectedOptionsText.forEach(item => {
+        const p = document.createElement('p')
+
+        p.textContent = item
+        p.classList.add('form__select-selected')
+        wrapper.appendChild(p)
+
+        if (isMultiple) {
+            const btn = document.createElement('button')
+            const closeImg = document.createElement('img')
+
+            btn.type = 'button'
+            closeImg.src = './../images/icons/close.svg'
+            btn.classList.add('form__select-selected-button')
+            p.classList.add('form__select-selected-multiple')
+            p.appendChild(btn)
+            btn.appendChild(closeImg)
+        }
+    })
+}
+
+displaySelected('form-responsible', 'form-responsible-wrapper')
+displaySelected('form-group', 'form-group-wrapper')
+displaySelected('form-coordinating', 'form-coordinating-wrapper')
+displaySelected('form-who-opened', 'form-who-opened-wrapper')
+displaySelected('form-created-by', 'form-created-by-wrapper')
